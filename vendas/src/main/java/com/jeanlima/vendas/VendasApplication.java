dd6436b63bc996c8ce2b8c096632289c86287a5d
jeanmmlima <jeanmariomlima@gmail.com>
2022-08-02 19:37:32 -0300

    get mapp para hello world

--- vendas/src/main/java/com/jeanlima/vendas/VendasApplication.java
+++ vendas/src/main/java/com/jeanlima/vendas/VendasApplication.java
@@ -2,9 +2,17 @@ package com.jeanlima.vendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication //classe que vai iniciar um container spring
+@RestController //vem da dependencia starter-web
 public class VendasApplication {
+
+	@GetMapping("/hello")
+	public String helloWorld(){
+		return "hello world";
+	}
 
 	public static void main(String[] args) {
 		//recebe a classe e os argumentos
