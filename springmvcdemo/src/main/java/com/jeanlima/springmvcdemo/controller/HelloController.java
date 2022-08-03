0c77b24a4d460c064f89743e1162ab693fb2a059
jeanmmlima <jeanmariomlima@gmail.com>
2022-08-03 18:45:10 -0300

    exemplo ola mundo

--- springmvcdemo/src/main/java/com/jeanlima/springmvcdemo/controller/HelloController.java
+++ springmvcdemo/src/main/java/com/jeanlima/springmvcdemo/controller/HelloController.java
@@ -1,13 +1,20 @@
 package com.jeanlima.springmvcdemo.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    
+
+	@GetMapping("/")
+    public String shwoPage(){
+		return "main-menu";
+	}
     @GetMapping("/ola")
 	public String saudar(@RequestParam(name="name", required=false, defaultValue="Mundo") String name, Model model) {
 		model.addAttribute("name", name);
@@ -18,6 +25,29 @@ public class HelloController {
 	public String saudarhtml(@RequestParam(name="name", required=false, defaultValue="Mundo") String name, Model model) {
 		model.addAttribute("name", name);
 		return "ola";
+	}
+
+	@RequestMapping("/showForm")
+	public String showForm(){
+		return "helloworld-form";
+	}
+
+	@RequestMapping("/processForm")
+	public String processForm(HttpServletRequest request, Model model){
+
+		String paramNome = request.getParameter("studentName");
+		model.addAttribute("studentName", paramNome);
+		return "helloworld";
+	}
+
+	@RequestMapping("/processFormDois")
+	public String processFormDois(@RequestParam("studentName") String nome, Model model){
+
+		//String paramNome = request.getParameter("studentName");
+		nome = nome.toUpperCase();
+		String result = "Olá! " + nome;
+		model.addAttribute("msg", result);
+		return "helloworld";
 	}
 
 }
