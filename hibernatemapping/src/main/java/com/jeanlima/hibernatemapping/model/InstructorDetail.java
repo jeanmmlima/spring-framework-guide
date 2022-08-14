33a6d7be2e06435edeadb3c63af9b8b852475ebe
jeanmmlima <jeanmariomlima@gmail.com>
2022-08-14 08:44:41 -0300

    mapping a oneToOne relationship

--- hibernatemapping/src/main/java/com/jeanlima/hibernatemapping/model/InstructorDetail.java
+++ hibernatemapping/src/main/java/com/jeanlima/hibernatemapping/model/InstructorDetail.java
@@ -1,10 +1,12 @@
 package com.jeanlima.hibernatemapping.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -33,6 +35,18 @@ public class InstructorDetail {
 	
 	@Column(name="hobby")
 	private String hobby;
+
+	/* 
+	 * Para estabelecer um relacionamento BIDIRECIONAL
+	 * a partir dos detalhes no instrutor, consigo indentificar o instrutuor associado
+	 * 
+	 * mappedBy: associa a propriedade correspondente na classe Instructor
+	 * 
+	 * Também posso usar a propriedade cascade para replicar as operações associadas a instructorDetail para instructor
+	 */
+
+	@OneToOne(mappedBy = "instructorDetail",cascade = CascadeType.ALL)
+	 private Instructor instructor;
 	
 	public InstructorDetail() {
 		
@@ -71,6 +85,16 @@ public class InstructorDetail {
 	public String toString() {
 		return "InstructorDetail [id=" + id + ", youtubeChannel=" + youtubeChannel + ", hobby=" + hobby + "]";
 	}
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
+	
 		
 }
 
