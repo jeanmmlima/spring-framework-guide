e9484a5c6359d50be00122186c5468bd7a8d12d6
jeanmmlima <jeanmariomlima@gmail.com>
2022-08-25 21:55:57 -0300

    pimeiro hello world com rest controller

--- springbootexample/src/main/java/com/jeanlima/springbootexample/SpringbootexampleApplication.java
+++ springbootexample/src/main/java/com/jeanlima/springbootexample/SpringbootexampleApplication.java
@@ -2,13 +2,23 @@ package com.jeanlima.springbootexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
 
 /*
  * Anotada com @SpringBootAplication
  * classe que vai iniciar uma aplicação spring boot
  */
 @SpringBootApplication
+@RestController
 public class SpringbootexampleApplication {
+
+
+	@GetMapping("/hello")
+	public String helloWorld(){
+		return "Olá, mundo!";
+	}
 
 	public static void main(String[] args) {
 		/*
