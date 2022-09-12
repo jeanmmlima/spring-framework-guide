eb6ba9a78dd51f0c8f0e607962e3b0d137130d98
jeanmmlima <jeanmariomlima@gmail.com>
2022-09-12 16:28:39 -0300

    primeiro pedido rest api

---
+++ spring-rest-api/src/main/java/com/jeanlima/springrestapi/rest/dto/PedidoDTO.java
@@ -0,0 +1,17 @@
+package com.jeanlima.springrestapi.rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PedidoDTO {
+    private Integer cliente;
+    private BigDecimal total;
+    private List<ItemPedidoDTO> items;
+}
